d68bc5e9de719f9c2431c5d4300586b5d656c186
Stephane Landelle <slandelle+github@excilys.com>
2012-03-22 15:40:46 +0100

    Update sample for 1.1.0

--- sample/js/menu.js
+++ sample/js/menu.js
@@ -4,11 +4,11 @@ function setDetailsLinkUrl(){
 }
 
 function setDetailsMenu(){
-    $(".nav ul").append("<li><a href='request_authentication_page_before_login.html' >Authentication page before login</a></li><li><a href='request_authentication.html' >Authentication</a></li><li><a href='request_accounts.html' >Accounts</a></li><li><a href='request_operations.html' >Operations</a></li><li><a href='request_operations_data.html' >Operations data</a></li><li><a href='request_cards_operations.html' >Cards operations</a></li><li><a href='request_cards_operations_data.html' >Cards operations data</a></li><li><a href='request_perform_transfer.html' >Perform transfer</a></li><li><a href='request_performing_transfer.html' >Performing transfer</a></li><li><a href='request_logout.html' >Logout</a></li><li><a href='request_authentication_page_after_logout.html' >Authentication page after logout</a></li>");
+    $(".nav ul").append("<li><a href='request_authentication_page_before_login.html' >Authentication page before login</a></li><li><a href='request_authentication.html' >Authentication</a></li><li><a href='request_authentication_redirect_1.html' >Authentication Redirect 1</a></li><li><a href='request_operations.html' >Operations</a></li><li><a href='request_operations_data.html' >Operations data</a></li><li><a href='request_cards_operations.html' >Cards operations</a></li><li><a href='request_cards_operations_data.html' >Cards operations data</a></li><li><a href='request_perform_transfer.html' >Perform transfer</a></li><li><a href='request_accounts.html' >Accounts</a></li><li><a href='request_logout.html' >Logout</a></li><li><a href='request_logout_redirect_1.html' >Logout Redirect 1</a></li>");
 }
 
 function setGlobalMenu(){
-    $(".nav ul").append("<li><a href='active_sessions.html'>Active Sessions</a></li><li><a href='requests.html'>Requests per Second</a></li>");
+    $(".nav ul").append("<li><a href='active_sessions.html'>Active Sessions</a></li><li><a href='requests.html'>Requests / sec</a></li><li><a href='transactions.html'>Transactions / sec</a></li>");
 }
 
 function getLink(link){
