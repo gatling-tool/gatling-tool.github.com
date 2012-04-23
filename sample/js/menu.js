aeb486220d873b49e3d8c758ea221eb64ab4be03
Stephane Landelle <slandelle+github@excilys.com>
2012-04-23 15:05:31 +0200

    Update sample for 1.1.4

--- sample/js/menu.js
+++ sample/js/menu.js
@@ -1,10 +1,21 @@
 
 function setDetailsLinkUrl(){
-    $("#details_link").attr('href', 'request_authentication_page_before_login.html');
+    $("#details_link").attr('href', encodeURIComponent('request_authentication_page_before_login.html'));
 }
 
 function setDetailsMenu(){
-    $(".nav ul").append("<li><a href='request_authentication_page_before_login.html' >Authentication page before login</a></li><li><a href='request_authentication.html' >Authentication</a></li><li><a href='request_authentication_redirect_1.html' >Authentication Redirect 1</a></li><li><a href='request_operations.html' >Operations</a></li><li><a href='request_operations_data.html' >Operations data</a></li><li><a href='request_cards_operations.html' >Cards operations</a></li><li><a href='request_cards_operations_data.html' >Cards operations data</a></li><li><a href='request_perform_transfer.html' >Perform transfer</a></li><li><a href='request_accounts.html' >Accounts</a></li><li><a href='request_logout.html' >Logout</a></li><li><a href='request_logout_redirect_1.html' >Logout Redirect 1</a></li>");
+    var menu = $(".nav ul");
+    $("<a>").attr({href: encodeURIComponent('request_authentication_page_before_login.html')}).text('Authentication page before login').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_authentication.html')}).text('Authentication').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_authentication_redirect_1.html')}).text('Authentication Redirect 1').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_operations.html')}).text('Operations').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_operations_data.html')}).text('Operations data').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_cards_operations.html')}).text('Cards operations').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_cards_operations_data.html')}).text('Cards operations data').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_perform_transfer.html')}).text('Perform transfer').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_accounts.html')}).text('Accounts').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_logout.html')}).text('Logout').wrap("<li>").parent().appendTo(menu);
+    $("<a>").attr({href: encodeURIComponent('request_logout_redirect_1.html')}).text('Logout Redirect 1').wrap("<li>").parent().appendTo(menu);
 }
 
 function setGlobalMenu(){
@@ -13,7 +24,7 @@ function setGlobalMenu(){
 
 function getLink(link){
 	var a = link.split('/');
-	return (a.length<=1)? link : a[a.length-1].toLowerCase();
+	return (a.length<=1)? link : a[a.length-1];
 }
  
 function setActiveMenu(){
