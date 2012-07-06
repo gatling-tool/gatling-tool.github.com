99373efe86acfaf3d8d59a04d78a3b402072782d
Stephane Landelle <slandelle+github@excilys.com>
2012-07-06 11:37:28 +0200

    Update sample

---
+++ sample/js/stats.js
@@ -0,0 +1,463 @@
+var stats = {
+	global_information: {
+		numberOfRequests : {
+			total: "360000",
+			ok: "360000",
+			ko: "0"
+		},
+		minResponseTime : {
+			total: "1",
+			ok: "1",
+			ko: "-"
+		},
+		maxResponseTime : {
+			total: "571",
+			ok: "571",
+			ko: "-"
+		},
+		meanResponseTime : {
+			total: "26",
+			ok: "26",
+			ko: "-"
+		},
+		standardDeviation : {
+			total: "21",
+			ok: "21",
+			ko: "-"
+		},
+		percentiles1 : {
+			total: "419",
+			ok: "419",
+			ko: "-"
+		},
+		percentiles2 : {
+			total: "461",
+			ok: "461",
+			ko: "-"
+		},
+		group1 : {
+			name: "t < 800ms",
+			count: 360000,
+			percentage: 100
+		},
+		group2 : {
+			name: "800ms < t < 1200ms",
+			count: 0,
+			percentage: 0
+		},
+		group3 : {
+			name: "1200ms < t",
+			count: 0,
+			percentage: 0
+		},
+		group4 : {
+			name: "failed",
+			count: 0,
+			percentage: 0
+		},
+		meanNumberOfRequestsPerSecond: {
+			total: "617",
+			ok: "617",
+			ko: "-"
+		}
+	},	search_apple: {
+		numberOfRequests : {
+			total: "60000",
+			ok: "60000",
+			ko: "0"
+		},
+		minResponseTime : {
+			total: "3",
+			ok: "3",
+			ko: "-"
+		},
+		maxResponseTime : {
+			total: "459",
+			ok: "459",
+			ko: "-"
+		},
+		meanResponseTime : {
+			total: "33",
+			ok: "33",
+			ko: "-"
+		},
+		standardDeviation : {
+			total: "20",
+			ok: "20",
+			ko: "-"
+		},
+		percentiles1 : {
+			total: "285",
+			ok: "285",
+			ko: "-"
+		},
+		percentiles2 : {
+			total: "395",
+			ok: "395",
+			ko: "-"
+		},
+		group1 : {
+			name: "t < 800ms",
+			count: 60000,
+			percentage: 100
+		},
+		group2 : {
+			name: "800ms < t < 1200ms",
+			count: 0,
+			percentage: 0
+		},
+		group3 : {
+			name: "1200ms < t",
+			count: 0,
+			percentage: 0
+		},
+		group4 : {
+			name: "failed",
+			count: 0,
+			percentage: 0
+		},
+		meanNumberOfRequestsPerSecond: {
+			total: "107",
+			ok: "107",
+			ko: "-"
+		}
+	},	home: {
+		numberOfRequests : {
+			total: "60000",
+			ok: "60000",
+			ko: "0"
+		},
+		minResponseTime : {
+			total: "1",
+			ok: "1",
+			ko: "-"
+		},
+		maxResponseTime : {
+			total: "474",
+			ok: "474",
+			ko: "-"
+		},
+		meanResponseTime : {
+			total: "14",
+			ok: "14",
+			ko: "-"
+		},
+		standardDeviation : {
+			total: "18",
+			ok: "18",
+			ko: "-"
+		},
+		percentiles1 : {
+			total: "234",
+			ok: "234",
+			ko: "-"
+		},
+		percentiles2 : {
+			total: "312",
+			ok: "312",
+			ko: "-"
+		},
+		group1 : {
+			name: "t < 800ms",
+			count: 60000,
+			percentage: 100
+		},
+		group2 : {
+			name: "800ms < t < 1200ms",
+			count: 0,
+			percentage: 0
+		},
+		group3 : {
+			name: "1200ms < t",
+			count: 0,
+			percentage: 0
+		},
+		group4 : {
+			name: "failed",
+			count: 0,
+			percentage: 0
+		},
+		meanNumberOfRequestsPerSecond: {
+			total: "107",
+			ok: "107",
+			ko: "-"
+		}
+	},	go_to_next_page: {
+		numberOfRequests : {
+			total: "60000",
+			ok: "60000",
+			ko: "0"
+		},
+		minResponseTime : {
+			total: "3",
+			ok: "3",
+			ko: "-"
+		},
+		maxResponseTime : {
+			total: "444",
+			ok: "444",
+			ko: "-"
+		},
+		meanResponseTime : {
+			total: "32",
+			ok: "32",
+			ko: "-"
+		},
+		standardDeviation : {
+			total: "20",
+			ok: "20",
+			ko: "-"
+		},
+		percentiles1 : {
+			total: "303",
+			ok: "303",
+			ko: "-"
+		},
+		percentiles2 : {
+			total: "429",
+			ok: "429",
+			ko: "-"
+		},
+		group1 : {
+			name: "t < 800ms",
+			count: 60000,
+			percentage: 100
+		},
+		group2 : {
+			name: "800ms < t < 1200ms",
+			count: 0,
+			percentage: 0
+		},
+		group3 : {
+			name: "1200ms < t",
+			count: 0,
+			percentage: 0
+		},
+		group4 : {
+			name: "failed",
+			count: 0,
+			percentage: 0
+		},
+		meanNumberOfRequestsPerSecond: {
+			total: "107",
+			ok: "107",
+			ko: "-"
+		}
+	},	select_apple_network_server: {
+		numberOfRequests : {
+			total: "60000",
+			ok: "60000",
+			ko: "0"
+		},
+		minResponseTime : {
+			total: "2",
+			ok: "2",
+			ko: "-"
+		},
+		maxResponseTime : {
+			total: "571",
+			ok: "571",
+			ko: "-"
+		},
+		meanResponseTime : {
+			total: "32",
+			ok: "32",
+			ko: "-"
+		},
+		standardDeviation : {
+			total: "21",
+			ok: "21",
+			ko: "-"
+		},
+		percentiles1 : {
+			total: "319",
+			ok: "319",
+			ko: "-"
+		},
+		percentiles2 : {
+			total: "461",
+			ok: "461",
+			ko: "-"
+		},
+		group1 : {
+			name: "t < 800ms",
+			count: 60000,
+			percentage: 100
+		},
+		group2 : {
+			name: "800ms < t < 1200ms",
+			count: 0,
+			percentage: 0
+		},
+		group3 : {
+			name: "1200ms < t",
+			count: 0,
+			percentage: 0
+		},
+		group4 : {
+			name: "failed",
+			count: 0,
+			percentage: 0
+		},
+		meanNumberOfRequestsPerSecond: {
+			total: "107",
+			ok: "107",
+			ko: "-"
+		}
+	},	home_redirect_1: {
+		numberOfRequests : {
+			total: "60000",
+			ok: "60000",
+			ko: "0"
+		},
+		minResponseTime : {
+			total: "4",
+			ok: "4",
+			ko: "-"
+		},
+		maxResponseTime : {
+			total: "333",
+			ok: "333",
+			ko: "-"
+		},
+		meanResponseTime : {
+			total: "16",
+			ok: "16",
+			ko: "-"
+		},
+		standardDeviation : {
+			total: "18",
+			ok: "18",
+			ko: "-"
+		},
+		percentiles1 : {
+			total: "248",
+			ok: "248",
+			ko: "-"
+		},
+		percentiles2 : {
+			total: "304",
+			ok: "304",
+			ko: "-"
+		},
+		group1 : {
+			name: "t < 800ms",
+			count: 60000,
+			percentage: 100
+		},
+		group2 : {
+			name: "800ms < t < 1200ms",
+			count: 0,
+			percentage: 0
+		},
+		group3 : {
+			name: "1200ms < t",
+			count: 0,
+			percentage: 0
+		},
+		group4 : {
+			name: "failed",
+			count: 0,
+			percentage: 0
+		},
+		meanNumberOfRequestsPerSecond: {
+			total: "107",
+			ok: "107",
+			ko: "-"
+		}
+	},	cancel: {
+		numberOfRequests : {
+			total: "60000",
+			ok: "60000",
+			ko: "0"
+		},
+		minResponseTime : {
+			total: "4",
+			ok: "4",
+			ko: "-"
+		},
+		maxResponseTime : {
+			total: "453",
+			ok: "453",
+			ko: "-"
+		},
+		meanResponseTime : {
+			total: "33",
+			ok: "33",
+			ko: "-"
+		},
+		standardDeviation : {
+			total: "21",
+			ok: "21",
+			ko: "-"
+		},
+		percentiles1 : {
+			total: "285",
+			ok: "285",
+			ko: "-"
+		},
+		percentiles2 : {
+			total: "401",
+			ok: "401",
+			ko: "-"
+		},
+		group1 : {
+			name: "t < 800ms",
+			count: 60000,
+			percentage: 100
+		},
+		group2 : {
+			name: "800ms < t < 1200ms",
+			count: 0,
+			percentage: 0
+		},
+		group3 : {
+			name: "1200ms < t",
+			count: 0,
+			percentage: 0
+		},
+		group4 : {
+			name: "failed",
+			count: 0,
+			percentage: 0
+		},
+		meanNumberOfRequestsPerSecond: {
+			total: "107",
+			ok: "107",
+			ko: "-"
+		}
+	}}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
